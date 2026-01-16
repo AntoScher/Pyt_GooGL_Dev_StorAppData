562e59c7e28f1ab4f695e51b4ad764a4d0978a90
AntoScher <asc8er@gmail.com>
2026-01-16 18:28:51 +0300

    lab2-1

---
+++ main3.py
@@ -0,0 +1,243 @@
+from flask import current_app, Flask, redirect, render_template
+from flask import request, url_for, session
+import booksdb
+import storage
+import secrets
+import oauth
+import translate
+import profiledb
+import json
+import os
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+app.config.update(
+    SECRET_KEY=secrets.get_secret('flask-secret-key'),
+    MAX_CONTENT_LENGTH=8 * 1024 * 1024,
+    ALLOWED_EXTENSIONS=set(['png', 'jpg', 'jpeg', 'gif']),
+    CLIENT_SECRETS=json.loads(secrets.get_secret('bookshelf-client-secrets')),
+    SCOPES=[
+        'openid',
+        'https://www.googleapis.com/auth/contacts.readonly',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+    EXTERNAL_HOST_URL=os.getenv('EXTERNAL_HOST_URL'),
+)
+app.debug = True
+app.testing = False
+
+def upload_image_file(img):
+    """
+    Upload the user-uploaded file to Cloud Storage and retrieve its
+    publicly accessible URL.
+    """
+    if not img:
+        return None
+
+    public_url = storage.upload_file(
+        img.read(),
+        img.filename,
+        img.content_type
+    )
+
+    current_app.logger.info(
+        'Uploaded file %s as %s.', img.filename, public_url)
+
+    return public_url
+
+def logout_session():
+    """
+    Clears known session items.
+    """
+    session.pop('credentials', None)
+    session.pop('user', None)
+    session.pop('state', None)
+    session.pop('error_message', None)
+    session.pop('login_return', None)
+    return
+
+
+def external_url(url):
+    """
+    Cloud Shell routes https://8080-***/ to localhost over http
+    This function replaces the localhost host with the configured scheme + hostname
+    """
+    external_host_url = current_app.config['EXTERNAL_HOST_URL']
+    if external_host_url is None:
+        # force https
+        if url.startswith('http://'):
+            url = f"https://{url[7:]}"
+        return url
+
+    # replace the scheme and hostname with the external host URL
+    parsed_url = urlparse(url)
+    replace_string = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    new_url = f"{external_host_url}{url[len(replace_string):]}"
+    return new_url
+
+
+@app.route('/error')
+def error():
+    """
+    Display an error.
+    """
+    if "error_message" not in session:
+        return redirect(url_for('.list'))
+
+    # render error
+    return render_template('error.html', error_message=session.pop('error_message', None))
+
+
+@app.route("/login")
+def login():
+    """
+    Login if not already logged in.
+    """
+    if not "credentials" in session:
+        # need to log in
+
+        current_app.logger.info('logging in')
+
+        # get authorization URL
+        authorization_url, state = oauth.authorize(
+            callback_uri=external_url(url_for('oauth2callback', _external=True)),
+            client_config=current_app.config['CLIENT_SECRETS'],
+            scopes=current_app.config['SCOPES'])
+
+        current_app.logger.info(f"authorization_url={authorization_url}")
+
+        # save state for verification on callback
+        session['state'] = state
+
+        return redirect(authorization_url)
+
+    # already logged in
+    return redirect(session.pop('login_return', url_for('.list')))
+
+
+@app.route("/oauth2callback")
+def oauth2callback():
+    """
+    Callback destination during OAuth process.
+    """
+    # check for error, probably access denied by user
+    error = request.args.get('error', None)
+    if error:
+        session['error_message'] = f"{error}"
+        return redirect(url_for('.error'))
+
+    # handle the OAuth2 callback
+    credentials, user_info = oauth.handle_callback(
+        callback_uri=external_url(url_for('oauth2callback', _external=True)),
+        client_config=current_app.config['CLIENT_SECRETS'],
+        scopes=current_app.config['SCOPES'],
+        request_url=external_url(request.url),
+        stored_state=session.pop('state', None),
+        received_state=request.args.get('state', ''))
+
+    session['credentials'] = credentials
+    session['user'] = user_info
+    current_app.logger.info(f"user_info={user_info}")
+
+    return redirect(session.pop('login_return', url_for('.list')))
+
+
+@app.route("/logout")
+def logout():
+    """
+    Log out and return to root page.
+    """
+    logout_session()
+    return redirect(url_for('.list'))
+
+@app.route('/')
+def list():
+    books = booksdb.list()
+    return render_template('list.html', books=books)
+
+@app.route('/books/<book_id>')
+def view(book_id):
+    book = booksdb.read(book_id)
+    return render_template('view.html', book=book)
+
+@app.route('/books/add', methods=['GET', 'POST'])
+def add():
+    if "credentials" not in session:
+        session['login_return'] = url_for('.add')
+        return redirect(url_for('.login'))
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+
+        image_url = upload_image_file(request.files.get('image'))
+
+        # If an image was uploaded, update the data to point to the image.
+        if image_url:
+            data['imageUrl'] = image_url
+
+        book = booksdb.create(data)
+        return redirect(url_for('.view', book_id=book['id']))
+    return render_template('form.html', action='Add', book={})
+
+@app.route('/books/<book_id>/edit', methods=['GET', 'POST'])
+def edit(book_id):
+    if "credentials" not in session:
+        session['login_return'] = url_for('.edit', book_id=book_id)
+        return redirect(url_for('.login'))
+    book = booksdb.read(book_id)
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+ 
+        image_url = upload_image_file(request.files.get('image'))
+
+        # If an image was uploaded, update the data to point to the image.
+        if image_url:
+            data['imageUrl'] = image_url
+            
+        book = booksdb.update(data, book_id)
+        return redirect(url_for('.view', book_id=book['id']))
+    return render_template('form.html', action='Edit', book=book)
+
+@app.route('/books/<book_id>/delete')
+def delete(book_id):
+    if "credentials" not in session:
+        session['login_return'] = url_for('.view', book_id=book_id)
+        return redirect(url_for('.login'))
+    booksdb.delete(book_id)
+    return redirect(url_for('.list'))
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    """
+    If GET, show the form to collect updated details for the user profile.
+    If POST, update the profile based on the specified form.
+    """
+    # must be logged in
+    if "credentials" not in session:
+        session['login_return'] = url_for('.profile')
+        return redirect(url_for('.login'))
+
+    # read existing profile
+    email = session['user']['email']
+    profile = profiledb.read(email)
+
+    # Save details if form was posted
+    if request.method == 'POST':
+
+        # get book details from form
+        data = request.form.to_dict(flat=True)
+
+        # update profile
+        profiledb.update(data, email)
+        session['preferred_language'] = data['preferredLanguage']
+
+        # return to root
+        return redirect(url_for('.list'))
+
+    # render form to update book
+    return render_template('profile.html', action='Edit',
+        profile=profile, languages=translate.get_languages())
+
+# this is only used when running locally
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
